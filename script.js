54a672ccf728d47f7185c577dc074bc806ed6809
Александр Ш <method4354@yandex.ru>
2023-09-16 01:09:51 +0500

    Search word

---
+++ script.js
@@ -0,0 +1,34 @@
+const input = document.getElementById("input");
+const infoText = document.getElementById("info-text");
+const meaningContainer = document.getElementById("meaning-container");
+const title = document.getElementById("title");
+const meaningEl = document.getElementById("meaning");
+
+
+async function request(word) {
+    infoText.style.display = "block";
+    meaningContainer.style.display = "none";
+    infoText.innerText = `Searching the meaning of "${word}"`;
+
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+    try {
+        const response = await fetch(url)
+        let result = await response.json()
+        infoText.style.display = "none";
+        meaningContainer.style.display = "block";
+        title.innerText = result[0].word;
+        meaningEl.innerText = result[0].meanings[0].definitions[0].definition;
+    }
+    catch (error) {
+        console.log(error)
+        infoText.innerText = `Error happened, try again later`
+    }
+
+}
+
+input.addEventListener("keyup", (e) => {
+    if (e.target.value && e.key === "Enter") {
+        request(e.target.value);
+    }
+});
